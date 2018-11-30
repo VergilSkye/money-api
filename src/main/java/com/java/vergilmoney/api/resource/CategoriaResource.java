6463eb0f713ce2360abe9172c08f92e8dc6f6e5e
Virgilio <virgiliosps@gmail.com>
2018-11-29 23:05:04 -0200

    Adicionando Rotas POST/categorias e GET/categorias/:id  :shipit:

--- src/main/java/com/java/vergilmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/java/vergilmoney/api/resource/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.java.vergilmoney.api.resource;
 import com.java.vergilmoney.api.model.Categoria;
 import com.java.vergilmoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,12 +22,33 @@ public class CategoriaResource {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public List<Categoria> listar(){
+    public List<Categoria> listar() {
         return categoriaRepository.findAll();
+
     }
 
-    @GetMapping("/outro")
-    public String outro(){
-        return "ok";
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+
+        Optional<Categoria> cat = categoriaRepository.findById(codigo);
+        if(cat.isPresent()){
+            return cat.get();
+        }
+        else{
+            return null;
+        }
+
+
     }
 }
