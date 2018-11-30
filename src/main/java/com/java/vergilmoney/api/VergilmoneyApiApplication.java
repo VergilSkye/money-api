86dd7ab6f579b8113e467a57b69bd8dd96ed5ff5
Virgilio <virgiliosps@gmail.com>
2018-11-29 22:07:36 -0200

    Criando Projeto Spring :sparkles:

---
+++ src/main/java/com/java/vergilmoney/api/VergilmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.vergilmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VergilmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VergilmoneyApiApplication.class, args);
+	}
+
+}
