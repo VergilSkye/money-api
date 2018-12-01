a277a696fcb57fb02ab0a5a34272d60ffde7c81e
Virgilio <virgiliosps@gmail.com>
2018-11-30 23:43:17 -0200

    Adicionando modelo Pessoa e Endereco para o recurso pessoas :sparkles:

---
+++ src/main/java/com/java/vergilmoney/api/model/Pessoa.java
@@ -0,0 +1,56 @@
+package com.java.vergilmoney.api.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @Embedded
+    private Endereco endereco;
+
+    @NotNull
+    private Boolean ativo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
