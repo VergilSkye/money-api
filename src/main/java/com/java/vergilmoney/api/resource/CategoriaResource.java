54385f6371be47355b7bf657d453a6ff37b6dfa4
Virgilio <virgiliosps@gmail.com>
2018-11-30 23:41:47 -0200

    Refatorando recurso categoria :hammer:

--- src/main/java/com/java/vergilmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/java/vergilmoney/api/resource/CategoriaResource.java
@@ -1,20 +1,17 @@
 package com.java.vergilmoney.api.resource;
 
+import com.java.vergilmoney.api.event.RecursoCriadoEvent;
 import com.java.vergilmoney.api.model.Categoria;
 import com.java.vergilmoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +23,9 @@ public class CategoriaResource {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
@@ -36,23 +36,22 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getId()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
 
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
 
     @GetMapping("/{codigo}")
-    public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 
         Optional<Categoria> cat = categoriaRepository.findById(codigo);
-        if(cat.isPresent()){
-            return cat.get();
-        }
-        else{
-            return null;
+        if (cat.isPresent()) {
+            Categoria categoria = cat.get();
+            return ResponseEntity.ok(categoria);
+        } else {
+            return ResponseEntity.notFound().build();
         }
 
 
